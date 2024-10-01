b6bc86bc619a5c3845178a53014e8a7cc6638189
FerchuTM <fer.win8.2014@gmail.com>
2024-09-30 23:35:12 -0300

    correcion
    corrección de tipeo

---
+++ src/index.js
@@ -0,0 +1,25 @@
+<<<<<<< Updated upstream
+=======
+import Escena1 from './model/Escena1.js'
+import GameOver from './model/GameOver.js';
+import Victoria from './model/Victoria.js';
+
+import Escena2 from './model/Escena2.js';
+import Inicio from './model/Inicio.js';
+import Final from './model/Final.js';
+
+let config={
+    type:Phaser.AUTO,
+    width:800,
+    height:600,
+    physics:{
+        default:'arcade',
+        arcade:{
+            gravity:{y:0},
+            debug:false
+        }
+    },
+    scene:[Inicio,Escena1,GameOver,Victoria,Escena2,Final]
+};
+let game = new Phaser.Game(config);
+>>>>>>> Stashed changes
